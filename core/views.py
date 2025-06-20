3688c6f49e164425c04438c6fd4cbc8b1bd03807
Sahil-Ishmam <sahiluddinishmam@gmail.com>
2025-06-21 02:50:14 +0600

    rest of the view are added

--- core/views.py
+++ core/views.py
@@ -33,7 +33,7 @@ def course_list_create(request):
         if request.user.role  in ['admin','student']:
             courses = Course.objects.all()
         elif request.user.role == 'teacher':
-            courses = Course.objects.filter(teacher=request.user)
+            courses = Course.objects.filter(instructor_id=request.user)
         else:
             return Response({"detail":"you are not authorized to view this page"}, status=403)
 
@@ -45,8 +45,114 @@ def course_list_create(request):
         if request.user.role != 'teacher':
             return Response({"detail":"only teacher can create Course"},status=403)
         serializer = CourseSerializers(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save(instructor_id=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    return Response({'detail':'please login'})
+
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def course_detail(request,pk):
+    try:
+        course = Course.objects.get(pk=pk)
+    except Course.DoesNotExist:
+        return Response({'detail':'Course Not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        if request.user.role =='admin' or request.user.role == course.instructor_id:
+            serializer = CourseSerializers(course)
+            return Response(serializer.data)
+        return Response({'detail':'Permission Denied'},status=403)
+    elif request.method == 'PUT':
+        if request.user.role != 'teacher' or request.user != course.instructor_id:
+            return Response({'detail':'only course teacher can update the course'}, status=403)
+        serializer = CategorySerializers(Course,data=request.data)
+        if serializer.is_valid():
+            serializer.save(instructor_id = request.user)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        if request.user.role != 'teacher' or request.user != course.instructor_id:
+            return Response({'detail':'only course teacher can delete the course'}, status=403)
+        course.delete()
+        return Response({'detail':'course deleted'},status=status.HTTP_204_NO_CONTENT)
+    
+
+
+
+@api_view(['GET','POST'])
+def lesson_list_create(request):
+    if request.method == 'GET':
+        categories = Lesson.objects.all()
+        serializer = LessonSerializers(categories, many=True)
+        return Response(serializer.data)
+    elif request.method=='POST':
+        serializer = LessonSerializers(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         
+        
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+
+
+
+@api_view(['GET','POST'])
+def material_list_create(request):
+    if request.method == 'GET':
+        categories = Material.objects.all()
+        serializer = MaterialSerializers(categories, many=True)
+        return Response(serializer.data)
+    elif request.method=='POST':
+        serializer = MaterialSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+
+
+
+@api_view(['GET','POST'])
+def enrollment_list_create(request):
+    if request.method == 'GET':
+        categories = Enrollment.objects.all()
+        serializer = EnrollmentSerializers(categories, many=True)
+        return Response(serializer.data)
+    elif request.method=='POST':
+        serializer = EnrollmentSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+
+@api_view(['GET','POST'])
+def questionanswer_list_create(request):
+    if request.method == 'GET':
+        categories = QuestionAnswer.objects.all()
+        serializer = QuestionAnswerSerializers(categories, many=True)
+        return Response(serializer.data)
+    elif request.method=='POST':
+        serializer = QuestionAnswerSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
---
+++ users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import user_list_create
+
+urlpatterns = [
+    path('auth/',user_list_create,name='user_list_create'),
+        
+
+
+]
